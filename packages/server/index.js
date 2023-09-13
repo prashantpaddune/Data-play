28121b49ef9964f669a35ff9f65af3bf7e765e02
prashantpaddune <padadunep@gmail.com>
2023-09-13 14:33:46 +0530

    server: /query api to execute query on DB.
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,1 +1,34 @@
-console.log("Data play");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+});
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/query', async (req, res) => {
+    const { query } = req.body;
+
+    try {
+        const result = await pool.query(query);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
