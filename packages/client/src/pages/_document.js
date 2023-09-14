8756b5ea45e86f5228149065011c2859da86b464
prashantpaddune <padadunep@gmail.com>
2023-09-14 11:57:20 +0530

    client: add meta tags
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- packages/client/src/pages/_document.js
+++ packages/client/src/pages/_document.js
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+        <title>Data Play - Your sandbox for SQL mastery</title>
+        <meta name="description" content="Your sandbox for SQL mastery"/>
+        <meta name="author" content="Prashant Paddune"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet" />
         <body>
         <Main />
